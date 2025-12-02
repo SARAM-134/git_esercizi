057b009e5c4c2a5c4031b6278727dc29be156b14
Sara <saramartino003@gmail.com>
2025-12-02 11:28:53 +0100

    add lettura da file

--- Quizzettone/main.py
+++ Quizzettone/main.py
@@ -4,12 +4,9 @@ answer_1="Sfera Ebbasta"
 answer_2="DPG-777"
 answer_3_="T3"
 answer_4="T4"
-def mostra_domanda() -> None:
-    print(f"""question
-    "A. "{answer_1}
-    "B. "{answer_2}
-    "C. "{answer_3_}
-    "D. "{answer_4}""")
+def mostra_domanda(domanda:str) -> None:
+    print(domanda)
+
 def raccogli_risposta() -> str:
     return input("Inserisci la tua risposta: ")
 
@@ -20,12 +17,42 @@ def valida_scelta(scelta:str)-> bool:
     else:
         return False
 
-def genera_feedback(scelta:str)->str:
-    if scelta=="A":
+def genera_feedback(scelta:str,risp_esatta:str)->str:
+    if scelta==risp_esatta:
         return "Hai indovinato"
     else:
         return "Non hai indovinato. Ritenta"
-mostra_domanda()
-risposta=raccogli_risposta()
-print(valida_scelta(risposta))
+    
 
+def leggi_da_file()->str:
+    with open("domanda1.txt") as file:
+        cont=file.read()
+    return cont
+            
+def estrai_domanda(contenuto:str,index:int)->str:
+    domanda=contenuto[0:index]
+    return domanda
+
+def estrai_risposta(contenuto:str, index:int)->str:
+    risposta=contenuto[index+1:index+2]
+    return risposta
+def estrai_index(contenuto:str)->int:
+    return contenuto.index('£')
+def is_corretta(scelta,risposta_esatta)->bool:
+    return scelta==risposta_esatta
+def main():
+    cont=leggi_da_file()
+    index:int=estrai_index(cont)
+    domanda=estrai_domanda(cont,index)
+    risposta=estrai_risposta(cont,index)
+    while True:
+        mostra_domanda(domanda)
+        risp=raccogli_risposta()
+        print(risp)
+        if valida_scelta(risp)==True:
+            print(genera_feedback(risp,risposta))
+        if is_corretta(risp,risposta):
+            break
+    
+
+main()
